b68462a4bbfb054a457e430fad1604efe0ab199e
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-11-09 23:41:46 +0100

    epic

--- src/commands/general.rs
+++ src/commands/general.rs
@@ -1,3 +1,5 @@
+use log::{debug, error, info, trace, warn};
+
 use serenity::model::channel::Message;
 use serenity::prelude::{EventHandler, Context};
 use serenity::framework::standard::{
@@ -16,7 +18,7 @@ group!({
 
 #[command]
 pub fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!");
+    let _ = msg.channel_id.say(&ctx.http, "Pong!");
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,78 @@
 use log::{debug, error, info, trace, warn, Level, LevelFilter};
 
-use std::{env, thread, time::Duration};
+use std::{env, thread, time::Duration, sync::Arc, collections::HashSet};
 
 use serenity::{
-    model::gateway::Ready,
+    model::{
+        gateway::Ready,
+        event::{
+            ResumedEvent,
+        },
+    },
     prelude::*,
-    client::Client,
-};
-use serenity::framework::standard::{
-    StandardFramework,
+    client::{
+        Client,
+        bridge::gateway::ShardManager,
+    },
+    framework::standard::StandardFramework,
 };
 
 pub mod commands;
 use commands::{
-    general::GENERAL_GROUP,
+    general::*,
 };
+
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
 
 struct Handler;
 
 impl EventHandler for Handler {
     fn ready(&self, _: Context, ready: Ready) {
-        if let Some(shard) = ready.shard {
-            //Note: Array index 0 is 0-indexed, while index 1 is 1-indexed. This follows Discord's behaviour.
-            info!(
-                "{} is connected on shard {}/{}",
-                ready.user.name,
-                shard[0] + 1,
-                shard[1],
-            );
-        }
+        info!("Succesfully connected as {}!", ready.user.name);
+    }
+
+    fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed!");
     }
 }
 
 fn main() {
     let mut builder = pretty_env_logger::formatted_builder();
-    builder.filter(Some("FukimageTokoyami"), LevelFilter::Info);
+    builder.filter(Some("Chomusuke"), LevelFilter::Info);
     builder.init();
 
     info!("Attempting login...");
 
     let token = include_str!("sensitive_data/token.txt");
 
-    info!("Fukimage Tokoyami is starting!");
+    info!("Chomusuke is starting!");
 
     let mut client = Client::new(&token, Handler).expect("Error creating the client!");
+
+    {
+        let mut data = client.data.write();
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+    }
+
+    let owners = match client.cache_and_http.http.get_current_application_info() {
+        Ok(info) => {
+            let mut set = HashSet::new();
+            set.insert(info.owner.id);
+
+            set
+        },
+        Err(why) => panic!("Couldn't get application info: {:?}", why),
+    };
+
     client.with_framework(StandardFramework::new()
-        .configure(|c| c.prefix("cs"))
+        .configure(|c| c.owners(owners).prefix("cs"))
         .group(&GENERAL_GROUP));
 
-    //Here we clone a lock to the Shard Manager, and then move it into a new thread.
-    //The thread will unlock the manager and print shards' status on a loop.
-    let manager = client.shard_manager.clone();
-
-    thread::spawn(move || {
-        loop {
-            thread::sleep(Duration::from_secs(30));
-
-            let lock = manager.lock();
-            let shard_runners = lock.runners.lock();
-
-            for (id, runner) in shard_runners.iter() {
-                info!(
-                    "Shard ID{} is {} with a latency of {:?}",
-                    id,
-                    runner.stage,
-                    runner.latency,
-                );
-            }
-        }
-    });
-
-    //Start `n` shards. Note that there is about a 5 second ratelimit period between starting threads
-    if let Err(why) = client.start_shards(20) {
+    if let Err(why) = client.start() {
         error!("Client error: {:?}", why);
     }
 }
