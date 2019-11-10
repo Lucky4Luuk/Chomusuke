1421c5aa186f13f42092ca57972aa5a031b42953
Spheya <33225496+Spheya@users.noreply.github.com>
2019-11-10 10:13:49 +0100

    Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then everything changed when the Fire Nation attacked.  Only the Avatar, master of all four elements , could stop them. But when the world needed him most, he vanished.  A hundred years passed and my brother and I discovered the new Avatar, an airbender named Aang, and although his airbending skills are great, he still has a lot to learn before he's ready to save anyone.  But I believe Aang can save the world.

--- src/commands/general/avatar.rs
+++ src/commands/general/avatar.rs
@@ -1,5 +1,7 @@
 use log::{debug, error, info, trace, warn};
 
+use regex::Regex;
+
 use serenity::model::channel::Message;
 use serenity::prelude::Context;
 use serenity::framework::standard::{
@@ -11,6 +13,40 @@ use serenity::framework::standard::{
 
 #[command]
 pub fn avatar(ctx: &mut Context, msg: &Message) -> CommandResult {
-    msg.channel_id.say(ctx, "Pong!");
+    let mut target = &msg.author;
+
+    // TODO: Put this in a seperate function in an util module
+    let regex = Regex::new(r"\s+").unwrap();
+    let formatted = regex.replace_all(msg.content["cs!".len()..].trim(), " ");
+    let arguments = formatted.split(' ');
+
+    println!("{:?}", arguments);
+
+    // TODO: Put this in a util module as well
+    {
+        // Check if an user was mentioned
+        let mentions = &msg.mentions;
+        if !mentions.is_empty() {
+            target = &mentions[0];
+        }
+    }
+
+    let profile_picture = target.avatar_url();
+
+    // Send a message if the target doesn't have a profile picture
+    if profile_picture.is_none() {
+        if (target == &msg.author) {
+            msg.channel_id.say(ctx, "Sorry, you don't have a profile picture!")
+            .expect("A channel became inaccessible during execution of a command!");
+        } else {
+            msg.channel_id.say(ctx, "Sorry, that user doesn't have a profile picture!")
+            .expect("A channel became inaccessible during execution of a command!");
+        }
+        return Ok(())
+    }
+
+    // Send the profile picture in the channel
+    msg.channel_id.say(ctx, profile_picture.unwrap())
+    .expect("A channel became inaccessible during execution of a command!");
     Ok(())
 }
